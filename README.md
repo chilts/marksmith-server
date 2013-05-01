2295e409eb31d90349166815c521302cbeab0fd6
Andrew Chilton <andychilton@gmail.com>
2013-05-01 18:25:23 +1200

    Initial commit which can process a site's worth of Markdown files

---
+++ README.md
@@ -0,0 +1,27 @@
+# Marksmith #
+
+Note: "Marksmith" is a cross between "MarkDown" and "Wordsmith" (not "Mark Smith").
+
+## What is it? ##
+
+Marksmith is a server which can look at a directory full of markdown files, assets and templates and serve them up on
+the port of your choosing.
+
+e.g.
+
+```bash
+$ node marksmith.js path/to/site 3000
+Server listening on port 3000
+```
+
+# Author #
+
+Written by [Andrew Chilton](http://chilts.org/)
+- [Blog](http://chilts.org/blog/)
+- [Twitter](https://twitter.com/andychilton).
+
+# License #
+
+* http://chilts.mit-license.org/2013/
+
+(Ends)
---
+++ marksmith.js
@@ -0,0 +1,81 @@
+// ----------------------------------------------------------------------------
+//
+// app.js - Marksmith's main server application.
+//
+// Copyright 2013 Andrew Chilton <andychilton@gmail.com>
+//
+// ----------------------------------------------------------------------------
+
+var http = require('http');
+var express = require('express');
+var marked = require('marked');
+
+// ----------------------------------------------------------------------------
+// some setup
+
+// express
+var app = express();
+
+// marked
+marked.setOptions({
+    gfm        : true,
+    tables     : true,
+    breaks     : false,
+    pedantic   : false,
+    sanitize   : true,
+    smartLists : true,
+    langPrefix : 'language-',
+});
+
+// incoming args: $ node app.js <directory> <port>
+var baseDir = process.argv[2].replace(/\/$/, '');
+var port    = process.argv[3] || 3000;
+
+// generate the other paths we need
+var staticDir = baseDir + '/public';
+var viewsDir  = baseDir + '/views';
+var siteDir   = baseDir + '/site';
+
+console.log('Using base   = ' + baseDir);
+console.log('Using static = ' + staticDir);
+console.log('Using views  = ' + viewsDir);
+console.log('Using site   = ' + siteDir);
+
+// ----------------------------------------------------------------------------
+
+// specific routes
+app.get('/', function(req, res) {
+    res.set('Content-Type', 'text/plain');
+    res.send('Hello, World!');
+});
+
+// specific routes
+app.get('/', function(req, res) {
+    res.set('Content-Type', 'text/plain');
+    res.send('Hello, World!');
+});
+
+app.get('/markdown', function(req, res) {
+    marked.setOptions({
+        gfm        : true,
+        tables     : true,
+        breaks     : false,
+        pedantic   : false,
+        sanitize   : true,
+        smartLists : true,
+        langPrefix : 'language-',
+    });
+    var html = marked('I am using __markdown__.');
+    res.send(html);
+});
+
+app.use(app.router);
+
+// ----------------------------------------------------------------------------
+
+var server = http.createServer(app);
+server.listen(port, function() {
+    console.log('Server listening on port ' + port);
+});
+
+// ----------------------------------------------------------------------------
