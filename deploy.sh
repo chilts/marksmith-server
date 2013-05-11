c4ddd844e6e2feec6e657ef39d3af3dc5835aa9f
Andrew Chilton <andychilton@gmail.com>
2013-05-11 23:44:16 +1200

    Add the deploy script and related config files

---
+++ deploy.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+## ----------------------------------------------------------------------------
+
+set -e
+
+WHOAMI=`id -un`
+GROUP=`id -gn`
+NODE=`which node`
+PWD=`pwd`
+
+## ----------------------------------------------------------------------------
+
+echo "Fetching new code ..."
+git pull --rebase
+echo
+
+## ----------------------------------------------------------------------------
+
+echo "Installing new npm packages ..."
+npm install
+echo
+
+## ----------------------------------------------------------------------------
+
+echo "Setting up various directories ..."
+sudo mkdir -p /var/log/marksmith/
+sudo chown $WHOAMI:$GROUP /var/log/marksmith/
+echo
+
+## ----------------------------------------------------------------------------
+
+echo "Adding the logrotate.d config ..."
+sudo cp etc/logrotate.d/marksmith /etc/logrotate.d/
+echo
+
+## ----------------------------------------------------------------------------
+
+echo "Making the /etc/marksmith.d/ directory ..."
+sudo mkdir /etc/marksmith.d/
+echo
+
+## ----------------------------------------------------------------------------
+
+echo "Not copying anything for proximity, since each marksmith site does that ..."
+echo
+
+## ----------------------------------------------------------------------------
+
+# add the upstart scripts
+echo "Copying upstart scripts ..."
+m4 \
+    -D __USER__=$USER \
+    -D __NODE__=$NODE \
+    -D __PWD__=$PWD \
+    etc/init/marksmith.conf.m4 | sudo tee /etc/init/marksmith.conf
+echo
+
+## ----------------------------------------------------------------------------
+
+# restart proximity
+echo "Restarting services ..."
+sudo service marksmith restart
+echo
+
+## --------------------------------------------------------------------------------------------------------------------
---
+++ etc/init/marksmith.conf.m4
@@ -0,0 +1,41 @@
+## ----------------------------------------------------------------------------
+#
+# Bookmarks
+#
+# http://kvz.io/blog/2009/12/15/run-nodejs-as-a-service-on-ubuntu-karmic/
+#
+## ----------------------------------------------------------------------------
+
+# info
+description "marksmith-server - Web server to serve up (markdown+assets+templates) -> (site+feeds) to your readers."
+author      "Andrew Chilton"
+
+# respawn this task
+start on runlevel [2345]
+respawn
+respawn limit 20 5
+stop on shutdown
+
+# allow opening of more than 1024 files
+limit nofile 4096 4096
+
+# set some environment variables
+env NODE_ENV=production
+
+# the script itself
+script
+
+    # quit the script if something goes wrong
+    set -e
+
+    # run the webserver as ubuntu
+    exec \
+        sudo -E -u __USER__ \
+        __NODE__ \
+        __PWD__/app.js 9000 \
+        >> /var/log/marksmith/web-access.log \
+        2>> /var/log/marksmith/web-error.log
+
+end script
+
+## ----------------------------------------------------------------------------
